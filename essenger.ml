360903aba47866e9d58e3b44a691b3ac62d271f7
William Soe <wms83@cornell.edu>
2019-10-30 11:49:46 -0400

    added skeletal functionality for each command, cleaned up interface for user side.

--- essenger.ml
+++ essenger.ml
@@ -1,27 +1,52 @@
 open Command
 
+(** [main] is the main interface for Essenger. It takes parsed commands from 
+    the command module and processes them to perform the proper function as 
+    specified by the command. *)
 let rec main () = 
   print_newline();
+  ANSITerminal.(print_string [cyan] "\nEssenger\n");
+  ANSITerminal.(print_string [white] 
+                  "Type @Help or @help to view supported commands.\n");
   print_string "> ";
   try 
     let command = read_line () |> Command.parse in
     match command with
-    | Send (r,m) -> (* Send message to server *) main ()
-    | Get r -> (* Get message history *) main ()
-    | Help -> (* Access Help Options *) main ()
+    | Send (r,m) -> (* Send message to server *) 
+      ANSITerminal.(print_string [cyan] 
+                      ("Recipient: " ^ r ^ " Message: " ^ m));
+      main ()
+    | Get r -> (* Get message history *) 
+      ANSITerminal.(print_string [cyan] 
+                      ("Getting message history with: " ^ r));
+      main ()
+    | Help -> (* Access Help Options *) 
+      ANSITerminal.(print_string [cyan]
+                      "\nSupported Commands: \n
+                    @<username> : Gets your message history with <username>\n
+                    @<username> <message> : Sends <message> to user <username>\n
+                    @Logout or @logout : Log out of Essenger.\n");
+      main ()
     | Logout -> (* Logout From Account *) 
-      ANSITerminal.(print_string [blue] 
+      ANSITerminal.(print_string [cyan] 
                       "\nLogging Out.\n");
       exit 0
   with
-  | Malformed -> print_string "Please Try Again.";
+  | Malformed -> print_string 
+                   "Please try again. Hint: Did you start with '@'?\n";
     main()
-  | Empty -> print_string "Please Try Again.";
+  | Empty -> print_string "Nothing was inputted, please try again.\n";
     main()
+  (*
+  |UnknownUser -> print_string "User not found.\n";
+  *)
 
-let login () = 
+(** [login] handles username and password input for the client-facing interface.
+    [login] takes in user input and passes that information as a JSON to the server,
+    which authenticates the login. If login fails, user is prompted to try again.*)
+let rec login () = 
   print_newline();
-  ANSITerminal.(print_string [blue] 
+  ANSITerminal.(print_string [cyan] 
                   "\nWelcome to Essenger, the Better Messenger.\n");
   print_string "Enter your username: ";
   let username_input = read_line () in
@@ -30,9 +55,16 @@ let login () =
   (* 
     if authenticated, then main, else try again
   *)
-  print_string ("Testing: " ^ username_input ^ " "^ password_input);
-  ANSITerminal.(print_string [blue] 
-                  "\nWelcome to Essenger.\n");
-  main()
+  (* THE FOLLOWING IS HARDCODED AUTHENTICATION *)
+  if username_input = "master" && password_input = "master" then 
+    (print_string ("Testing: " ^ username_input ^ " "^ password_input);
+     print_newline ();
+     ANSITerminal.(print_string [cyan] 
+                     ("\n"^username_input^", welcome to Essenger.\n"));
+     main ())
+  else
+    print_string "Incorrect login, try again.";
+  login ()
+(* END of Hardcode *)
 
 let () = login ()
