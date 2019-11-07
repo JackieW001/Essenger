f1e459b4ab3b2f07f1a899347b6427c6a268921e
JackieW001 <jwoo@stuy.edu>
2019-11-06 22:57:05 -0500

    authors and installing instructions

---
+++ authors.mli
@@ -0,0 +1,8 @@
+(**
+   Metadata about this submission.
+   @author Jackie Woo (jmw499), William Soe (wms83), Catie Rencricca (cr498)
+*)
+
+(** [hours_worked] is a list of the number of hours each team member
+    worked on this assignment. *)
+val hours_worked : int list
