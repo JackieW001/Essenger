ceabb72306384cdeed47626b3b198ecbadbaa224
William Soe <wms83@cornell.edu>
2019-11-08 10:58:35 -0500

    added guard so that a user cannot create an account for a username that already exists

--- essenger.ml
+++ essenger.ml
@@ -5,7 +5,7 @@ open Sha256
 (** [main] is the main interface for Essenger. It takes parsed commands from 
     the command module and processes them to perform the proper function as 
     specified by the command. *)
-let rec main () = 
+let rec main current_user () = 
   print_newline();
   ANSITerminal.(print_string [cyan] "\nEssenger\n");
   ANSITerminal.(print_string [white] 
@@ -19,19 +19,19 @@ let rec main () =
                       ("Recipient: " ^ r ^ "\nMessage: " ^ m));
       ANSITerminal.(print_string [red] 
                       ("\nUnimplemented."));
-      main ()
+      main current_user ()
     | Get r -> (* Get message history *) 
       ANSITerminal.(print_string [cyan] 
                       ("Getting message history with: " ^ r));
       ANSITerminal.(print_string [red] 
                       ("\nUnimplemented."));
-      main ()
+      main current_user ()
     | Friends -> (* Get List of friends *) 
       ANSITerminal.(print_string [cyan] 
                       ("Getting friends list."));
       ANSITerminal.(print_string [red] 
                       ("\nUnimplemented."));
-      main ()
+      main current_user ()
     | Help -> (* Access Help Options *) 
       ANSITerminal.(print_string [cyan]
                       "\nSupported Commands: \n
@@ -39,7 +39,7 @@ let rec main () =
               @<username> <message> : Sends <message> to user <username>\n
               @Friends or @friends : View who you've had conversations with.\n
               @Logout or @logout : Log out of Essenger.\n");
-      main ()
+      main current_user ()
     | Logout -> (* Logout From Account *) 
       ANSITerminal.(print_string [cyan] 
                       "\nLogging Out.\n");
@@ -47,9 +47,9 @@ let rec main () =
   with
   | Malformed -> print_string 
                    "Please try again. Hint: Did you start with '@'?\n";
-    main()
+    main current_user ()
   | Empty -> print_string "Nothing was inputted, please try again.\n";
-    main()
+    main current_user ()
   (*
   |UnknownUser -> print_string "User not found.\n";
   *)
@@ -58,7 +58,7 @@ let rec main () =
     [login] takes in user input and passes that information as a JSON to the server,
     which authenticates the login. If login fails, user is prompted to try again.*)
 let rec login () = 
-  print_newline();
+  print_newline ();
   ANSITerminal.(print_string [cyan] 
                   "\nWelcome to Essenger, the Better Messenger.\n");
   ANSITerminal.(print_string [cyan] 
@@ -66,34 +66,40 @@ let rec login () =
   let response = read_line () in
   if response = "y" then (
     print_string "\nEnter your username: ";
-    let username_input = read_line () in
+    let username_input = String.trim(read_line ()) in
     print_string "Enter your password: ";
-    let password_input = read_line () in
+    let password_input = String.trim(read_line ()) in
     (* 
     if authenticated, then main, else try again
   *)
     if Server.auth username_input password_input then 
       (ANSITerminal.(print_string [green] 
                        ("\n"^username_input^", welcome to Essenger."));
-       main ())
+       main username_input ())
     else
       ANSITerminal.(print_string [red] "\nIncorrect login, try again.");
     login ())
   else(
     if response = "n" then(
-      ANSITerminal.(print_string [cyan] 
+      ANSITerminal.(print_string [white] 
                       "\nPlease enter a username: ");
       let created_username = String.trim (read_line ()) in
-      ANSITerminal.(print_string [cyan]
-                      "\nPlease enter a password: ");
-      let created_password = String.trim(read_line ()) in
-      let init_ctx = init () in 
-      update_string init_ctx created_password; 
-      let hashed_password = to_hex (finalize init_ctx) in
-      Server.create_user created_username hashed_password |> Lwt_main.run;
-      (ANSITerminal.(print_string [green] 
-                       ("\n"^created_username^", welcome to Essenger.")));
-      main ()
+      if Server.user_exists created_username then(
+        ANSITerminal.(print_string [red]
+                        "\nUsername already exists. Please try again.\n");
+        login ()
+      )
+      else (
+        ANSITerminal.(print_string [white]
+                        "Please enter a password: ");
+        let created_password = String.trim(read_line ()) in
+        let init_ctx = init () in 
+        update_string init_ctx created_password; 
+        let hashed_password = to_hex (finalize init_ctx) in
+        Server.create_user created_username hashed_password |> Lwt_main.run;
+        (ANSITerminal.(print_string [green] 
+                         ("\n"^created_username^", welcome to Essenger.")));
+        main created_username ())
     )
     else(
       ANSITerminal.(print_string [red] ("\nUnrecognized. Please try again."));
