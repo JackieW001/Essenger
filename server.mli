672c4e18455dc1fe868aaee2470620475dea26c4
JackieW001 <jwoo@stuy.edu>
2019-12-04 20:28:05 -0500

    update documentation

--- server.mli
+++ server.mli
@@ -1,21 +1,3 @@
-(** 
-   Server that stores csv files of two user's converstation history. 
-   Csv files are named <user1>_<user2>.csv and stores the conversation
-   history between <user1> and <user2>.
-   Each rows of the csv will store:
-    1. the timestamp of when the message was sent as a string
-    2. the sender of the message
-    3. the message as a string
-
-   The server will be able to: 
-   - authenticate user login 
-   - parse a json file for the timestamp, sender, recipient, and message
-   - create a new csv file between two autheticated users if a conversation
-      was just started
-   - append new rows (aka messages) to an existing csv file
-   - send the last five messages from a sender to a recipient
-*)
-
 (** [sender] represents the username of the sender of a message. *)
 type sender = string 
 
